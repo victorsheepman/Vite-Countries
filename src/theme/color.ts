c72414f3d4ff7e0826e5348ca03540fafdae0c7b
victor_marquez <victorsheepman@gmail.com>
2024-04-22 16:27:20 -0400

    color-done

---
+++ src/theme/color.ts
@@ -0,0 +1,8 @@
+import { hsl } from 'csx';
+
+export const darkBlue = hsl(209, '23%', '22%');
+export const veryDarkBlue = hsl(207, '26%', '17%');
+export const veryDarkBlueText = hsl(200, '15%', '8%');
+export const darkGray = hsl(0, '0%', '52%');
+export const veryLightGray = hsl(0, '0%', '98%');
+export const white = hsl(0, '0%', '100%');
