561bd793f4d42737c9be521806412a5ff4d119ce
victor_marquez <victorsheepman@gmail.com>
2024-04-24 15:17:56 -0400

    mobile done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,21 @@ import { media, style } from 'typestyle'
 import { FillterRegion, SearchCountry } from '../components'
 
 export const Home = () => {
+    let list = [1,2,3,4,5,6,7,8]
   return (
     <div className={homeStyle}>
         <section className={homeFiltersStyle}>
             <SearchCountry />
             <FillterRegion />
+        </section>
+        <section className={homeListWrapperStyle}>
+            <div className={homeListStyle}>
+                {
+                    list.map(i=>(
+                        <div className={cardExample}></div>
+                    ))
+                }
+            </div>
         </section>
     </div>
   )
@@ -40,4 +50,35 @@ const homeFiltersStyle =  style(
             padding:'48px 80px'
         }
     ),
+)
+
+const homeListWrapperStyle = style(
+    {
+        width:'100%',
+        height:'auto',
+        padding:'0px 56px',
+        paddingBottom:'65px',
+        paddingTop:'32px'
+    }
+)
+
+const homeListStyle = style(
+    {
+        width:'100%',
+        height:'auto',
+        display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        gap:'40px',
+        backgroundColor:'black'
+    }
+)
+
+
+const cardExample = style(
+    {
+        width:'264px',
+        height:'336px',
+        backgroundColor:'red'
+    }
 )
