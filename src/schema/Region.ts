e927d3d709ba5c87ebc83ff5f72acc01fef51315
victor_marquez <victorsheepman@gmail.com>
2024-04-24 12:31:48 -0400

    added enum

---
+++ src/schema/Region.ts
@@ -0,0 +1,15 @@
+
+export interface RegionOption {
+    value:RegionEnum,
+    label:RegionEnum
+}
+
+
+enum RegionEnum {
+    Africa  = "Africa",
+    America = "America",
+    Asia    = "Asia",
+    Europe  = "Europe",
+    Oceania = "Oceania"
+}
+  
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,1 +1,2 @@
 export * from './Country'
+export * from './Region'
