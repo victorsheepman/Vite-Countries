dea62d62d033d8e89ffc45e65f57b9812ca6c0de
victor_marquez <victorsheepman@gmail.com>
2024-04-24 11:18:28 -0400

    added state in layout

--- src/container/Layout.tsx
+++ src/container/Layout.tsx
@@ -1,22 +1,15 @@
-import { ReactNode } from 'react'
-import { style } from 'typestyle'
-import { nunito, veryLightGray } from '../theme'
+import { ReactNode, useContext } from 'react'
+import { classes, style } from 'typestyle'
+import { darkBlue, nunito, veryLightGray } from '../theme'
 import { Header } from '.'
+import { CountryContext } from '../context'
 
 export const Layout = ({children}:{children:ReactNode}) => {
+  const {isDarkMode} = useContext(CountryContext)
   return (
-    <div className={layoutStyle}>
+    <div className={classes(style({fontFamily:nunito, backgroundColor: isDarkMode ? darkBlue : veryLightGray}))}>
         <Header/>
         {children}
     </div>
   )
 }
-
-
-const layoutStyle = style(
-  {
-    backgroundColor: veryLightGray,
-    fontFamily:nunito
-  },
-
-)
