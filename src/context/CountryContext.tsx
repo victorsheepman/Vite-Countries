0bd85c54270dc4674f586b754275dbd30a9c4b2f
victor_marquez <victorsheepman@gmail.com>
2024-04-25 12:13:44 -0400

    progress

--- src/context/CountryContext.tsx
+++ src/context/CountryContext.tsx
@@ -28,9 +28,11 @@ export const useCountryContext = () => {
     const [countries, setCountries] = useState<Country[]>([]);
     const [isDarkMode, setisDarkMode] = useState<boolean>(false)
     const [regionSelected, setRegionSelected] = useState<RegionEnum>(RegionEnum.America)
+
+    const filters = "name,capital,population,flags,region,cioc"
    
     const fetchApi = async ()=>{
-        const res = await fetch(`https://restcountries.com/v3.1/region/${regionSelected}?fields=name,capital,population,flags,region`);
+        const res = await fetch(`https://restcountries.com/v3.1/region/${regionSelected}?fields=${filters}`);
         const json = await res.json();
         setCountries(json)
     }
@@ -39,7 +41,7 @@ export const useCountryContext = () => {
         
         setRegionSelected(region as RegionEnum)//save region to fetch api function
 
-        const res = await fetch(`https://restcountries.com/v3.1/region/${region}?fields=name,capital,population,flags,region`);
+        const res = await fetch(`https://restcountries.com/v3.1/region/${region}?fields=${filters}`);
         const json = await res.json();
         setCountries(json)
     
@@ -57,7 +59,7 @@ export const useCountryContext = () => {
 
     const callApi= async (country:string)=>{
 
-        const res = await fetch(`https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,flags,region`);
+        const res = await fetch(`https://restcountries.com/v3.1/name/${country}?fields=${filters}`);
         
         if(res.status == 200) {
             const json = await res.json();
@@ -69,7 +71,7 @@ export const useCountryContext = () => {
     }
 
     const getCountryDetail = async (name:string)=>{
-        const res = await fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,region,subregion,tld,currencies,languages,borders,cioc`);  
+        const res = await fetch(`https://restcountries.com/v3.1/alpha/${name}?fields=${filters}`);  
         const json = await res.json();
         console.log(json);
     }
--- src/schema/Country.ts
+++ src/schema/Country.ts
@@ -4,6 +4,7 @@ export interface Country {
     capital:    string[];
     population: number;
     region:     string,
+    cioc:       string
 }
 
 type Flags = {
