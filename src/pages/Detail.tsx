5c8c8d089ae80712243c308f9acce215816f625f
victor_marquez <victorsheepman@gmail.com>
2024-04-25 12:09:08 -0400

    progress

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { CountryContext } from '../context'
 
 export const Detail = () => {
+  const { name } = useParams()
+  const { getCountryDetail } = useContext(CountryContext)
+
+  useEffect(() => {
+    if (name) {
+        getCountryDetail(name)
+    }
+  }, [])
+  
   return (
-    <div>Detail</div>
+    <div>{name}</div>
   )
 }
